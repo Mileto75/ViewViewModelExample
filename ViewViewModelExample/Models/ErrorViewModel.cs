e62aed44aba18882d306dde2bbafa48a1e516ea6
mileto1975 <mileto.di.marco@howest.be>
2019-09-11 14:00:42 +0200

    Add project files.

---
+++ ViewViewModelExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ViewViewModelExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
