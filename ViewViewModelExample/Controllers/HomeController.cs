b06710c5f959bb98ed1bbb2d54d65cc45ad08bd1
mileto1975 <mileto.di.marco@howest.be>
2019-09-11 16:02:08 +0200

    changed namespace to correct ViewModels

--- ViewViewModelExample/Controllers/HomeController.cs
+++ ViewViewModelExample/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ViewViewModelExample.Models;
+using ViewViewModelExample.ViewModels;
 
 namespace ViewViewModelExample.Controllers
 {
--- ViewViewModelExample/ViewModels/HomeGetGameVM.cs
+++ ViewViewModelExample/ViewModels/HomeGetGameVM.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ViewViewModelExample.Models
+namespace ViewViewModelExample.ViewModels
 {
     public class HomeGetGameVM
     {
