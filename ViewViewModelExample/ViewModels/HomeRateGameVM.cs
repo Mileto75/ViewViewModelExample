fe883b6b4b843c9daccd2d7071bbfd83c1bb5a44
mileto1975 <mileto.di.marco@howest.be>
2019-09-18 16:16:47 +0200

    Adds rategame view

---
+++ ViewViewModelExample/ViewModels/HomeRateGameVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewViewModelExample.ViewModels
+{
+    public class HomeRateGameVM
+    {
+        public int GameId { get; set; }
+        public string Gametitle { get; set; }
+        public int Gamerating { get; set; }
+    }
+}
