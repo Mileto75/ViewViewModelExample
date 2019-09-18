e5460088c77c00bdea54acbe39029934e08a31f4
mileto1975 <mileto.di.marco@howest.be>
2019-09-18 11:51:54 +0200

    Adds HomeGetGamesVM class

---
+++ ViewViewModelExample/ViewModels/HomeGetGamesVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Example.Games.Domain;
+namespace ViewViewModelExample.ViewModels
+{
+    public class HomeGetGamesVM
+    {
+        public IEnumerable<Game> games = new List<Game>();
+    }
+}
