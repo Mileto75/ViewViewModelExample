f8562e02854be91ff2f741ed5dd2d886e9d89ce3
mileto1975 <mileto.di.marco@howest.be>
2019-09-18 11:23:32 +0200

    Adds Gamerepository class

---
+++ Example.Games.Domain/Gamerepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Games.Domain
+{
+    class Gamerepository
+    {
+        private IEnumerable<Game> MyGames;
+        public IEnumerable<Game> GetGames()
+        {
+            MyGames = new List<Game>
+            {
+                new Game{GameId=0,GameTitle="Wolfenstein",GamePublisher="Ubisoft" },
+                new Game{GameId=1,GameTitle="Fifa",GamePublisher="EA" },
+                new Game{GameId=2,GameTitle="Doom",GamePublisher="Bethesta" },
+                new Game{GameId=3,GameTitle="Call of duty",GamePublisher="Ubisoft" }
+            };
+            return MyGames;
+        }
+
+    }
+}
